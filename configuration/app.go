bfe8ffa3caeb1e86a72e8c294e2f969d8127a7ed
hakrithy <hakrithykim2020@gmail.com>
2025-07-24 09:57:56 +0700

    add code

---
+++ configuration/app.go
@@ -0,0 +1,36 @@
+package configuration
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+type AppConfiguration struct {
+	AppHost string
+	AppPort int
+}
+
+func GetenvInt(key string, defaultValue int) int {
+	valueStr := os.Getenv(key)
+	value, err := strconv.Atoi(valueStr)
+	if err != nil {
+		return defaultValue
+	}
+	return value
+}
+
+func NewConfiguration() *AppConfiguration {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file %v", err)
+	}
+	host := os.Getenv("API_HOST")
+	port := GetenvInt("API_PORT", 8888)
+	return &AppConfiguration{
+		AppHost: host,
+		AppPort: port,
+	}
+}
